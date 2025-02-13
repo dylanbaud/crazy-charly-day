565d46aceb623c491414bf704f56c4de0a3e8d3b
Baudson Dylan <dylan.baudson1@etu.univ-lorraine.fr>
2025-02-13 10:50:37 +0100

    prisma

--- backend/needs.js
+++ backend/needs.js
@@ -1,14 +1,16 @@
-function initNeeds(app) {
+function initNeeds(app, prisma) {
     //get ALL needs
-    app.get('/needs', (req, res) => {
-        const needs = {};
+    app.get('/needs', async (req, res) => {
+        await prisma.need.findMany().then(needs => {
+            res.json(needs);
+        });
         res.json(needs);
     });
 
     app.post('/create-need', (req, res) => {
         const {description, skill, customer} = req.body;
 
-        if(description==null || skill ==null || customer == null) {
+        if (description == null || skill == null || customer == null) {
             res.status(400).json({
                 message: 'Missing arguments',
             })
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const {initNeeds} = require("./needs");
 
-
 const express = require('express');
 const app = express();
 
 const port = 4000;
+
+const {PrismaClient} = require('@prisma/client');
+
+const prisma = new PrismaClient();
 
 app.use(express.json());
 
@@ -12,4 +15,4 @@ app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
-initNeeds(app);
+initNeeds(app, prisma);
