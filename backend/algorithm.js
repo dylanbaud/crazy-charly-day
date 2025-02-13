c9dfa2752546fc6815060a93cfc788e6e4b4922c
ZackPedretti <zacklycee.bp@gmail.com>
2025-02-13 22:13:22 +0100

    Algorithmes créent des tâches

--- backend/algorithm.js
+++ backend/algorithm.js
@@ -10,12 +10,39 @@ async function fetchData(url) {
 async function handleAlgorithm(algoClass, res) {
     try {
         const [needs, employees] = await Promise.all([
-            fetchData("http://localhost:4000/free-needs"),
-            fetchData("http://localhost:4000/free-employees")
+            fetchData("http://localhost:45555/free-needs"),
+            fetchData("http://localhost:45555/free-employees")
         ]);
 
         const algo = new algoClass();
-        res.json(algo.optimizeAssignments(needs, employees));
+
+        const assignments = algo.optimizeAssignments(needs, employees)
+
+        for (const assignment of assignments) {
+            const { needId, employeeId } = assignment;
+
+            const startDate = new Date();
+            startDate.setHours(startDate.getHours() + 1);
+            const finishDate = new Date(startDate);
+            finishDate.setDate(finishDate.getDate() + 1);
+
+            try {
+                await fetch("http://localhost:45555/tasks", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        need_id: needId,
+                        employee_id: employeeId,
+                        start_date: startDate,
+                        finish_date: finishDate
+                    })
+                });
+            } catch (error) {
+                console.error("Erreur lors de la création de la tâche :", error);
+            }
+        }
+
+        res.json(assignments);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const port = 4000;
+const port = 45555;
 
 const prisma = new PrismaClient();
 
