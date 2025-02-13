f29230c055b41877025a24351fe6621ae1a38388
Gaetan Pinot <gaetan.pinot6@etu.univ-lorraine.fr>
2025-02-13 10:48:32 +0100

    init front

---
+++ frontend/src/services/httpClient.js
@@ -0,0 +1,42 @@
+const BASE_URL = 'http://localhost:3001'
+const request = async (
+  endpoint,
+  method = 'GET',
+  baseUrl = true,
+  body = null,
+  isAuthRequest = false,
+) => {
+  // Définir les en-têtes
+  const headers = {
+    'Content-Type': 'application/json',
+  }
+  // Configurer la requête
+  const config = {
+    method,
+    headers,
+    ...(body && { body: JSON.stringify(body) }),
+  }
+  try {
+    // Envoyer la requête
+    const url = baseUrl ? `${BASE_URL}${endpoint}` : endpoint
+    const response = await fetch(url, config)
+    if (response.status === 403 || response.status === 401) {
+      // Token expired or invalid, remove token and redirect to login
+      window.location.href = '/login'
+      return
+    }
+    // Gérer la réponse
+    if (!response.ok) {
+      const errorBody = await response.json()
+      throw new Error(errorBody.message || 'Something went wrong')
+    }
+    // Vérifier si la réponse a du contenu
+    const contentType = response.headers.get('content-type')
+    return contentType && contentType.includes('application/json') ? await response.json() : null
+  } catch (error) {
+    // Gestion des erreurs
+    console.error('API Error:', error)
+    throw error
+  }
+}
+export {}
