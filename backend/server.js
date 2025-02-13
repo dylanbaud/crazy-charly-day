e30e17d3b16479fceaf6e81ebd7672315be2fdc1
Antoine FONTANEZ <antoinef252@gmail.com>
2025-02-13 09:53:33 +0100

    Suppression lignes inutiles server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,8 @@
-const path = require("path");
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
-
-const io = require('socket.io').listen(server, {
-    pingInterval: 30000,
-    pingTimeout: 5000,
-});
 
 const port = 3000;
 
-// -- Used to solve some problems ----------------
-//app.use(express.static(path.join(__dirname)));
 app.use(express.json());
 
 app.listen(port, () => {
