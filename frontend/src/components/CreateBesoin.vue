c0c6397d40150ecffacb6ef07231db9f3d678f62
Gaetan Pinot <gaetan.pinot6@etu.univ-lorraine.fr>
2025-02-13 11:25:40 +0100

    formulaire

--- frontend/src/components/CreateBesoin.vue
+++ frontend/src/components/CreateBesoin.vue
@@ -13,21 +13,28 @@ export default {
 <template>
   <h2> Créer un besoin en personnel</h2>
 
-  <label for="email_entreprise"> Email de l'entreprise </label>
-  <input v-model="email_entreprise" type="email" id="email_entreprise" name="email_entreprise" placeholder="email"
-    required>
+  <div id="formulaire">
+    <label for="email_entreprise"> Email de l'entreprise </label>
+    <input v-model="email_entreprise" type="email" id="email_entreprise" name="email_entreprise" placeholder="email"
+      required>
 
-  <label for="description_besoin">Description du besoin </label>
-  <textarea v-model="description_besoin" id="description_besoin" name="description_besoin" placeholder="description"
-    required></textarea>
+    <label for="description_besoin">Description du besoin </label>
+    <textarea v-model="description_besoin" id="description_besoin" name="description_besoin" placeholder="description"
+      required></textarea>
 
-  <label for="competence_IF"> Informatique </label>
-  <input v-model="competence_besoin" type="radio" id="competence_BR" name="competence_besoin" value="BR">
-  <input v-model="competence_besoin" type="radio" id="competence_JD" name="competence_besoin" value="JD">
-  <input v-model="competence_besoin" type="radio" id="competence_MN" name="competence_besoin" value="MN">
-  <input v-model="competence_besoin" type="radio" id="competence_IF" name="competence_besoin" value="IF">
-  <input v-model="competence_besoin" type="radio" id="competence_AD" name="competence_besoin" value="AD">
+    <label for="competence_BR">Bricolage</label>
+    <input v-model="competence_besoin" type="radio" id="competence_BR" name="competence_besoin" value="BR">
+    <label for="competence_JD"> Jardinage </label>
+    <input v-model="competence_besoin" type="radio" id="competence_JD" name="competence_besoin" value="JD">
+    <label for="competence_MN"> Ménage </label>
+    <input v-model="competence_besoin" type="radio" id="competence_MN" name="competence_besoin" value="MN">
+    <label for="competence_IF"> Informatique </label>
+    <input v-model="competence_besoin" type="radio" id="competence_IF" name="competence_besoin" value="IF">
+    <label for="competence_AD">Accompagnement dans les démarches administratives</label>
+    <input v-model="competence_besoin" type="radio" id="competence_AD" name="competence_besoin" value="AD">
 
+  </div>
+  <button @click="creerBesoin">Créer le besoin</button>
 </template>
 
 <style lang="scss" scoped></style>
